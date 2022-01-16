8b9631831c30db13e73b1a604b3be61956957f92
devzonayed <zonayedofficial@gmail.com>
2022-01-16 22:04:46 +0600

    complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,86 @@
-const countSystem = qs('#countSystem');
-const inputTime = qs('.input-time');
-const fromTime = qs('#fromtime');
+countStart.onclick = () => {
+  if (countSystem.value == "backwards") {
+    if (fromTime.value == "") {
+        fromTime.style.cssText = `
+        border:2px solid red;
+    `;
+    } else {
+      backward();
+    countStop.removeAttribute("disabled");
+      timerstatus = true;
+      btnstart();
+    }
+  } else {
+    forward();
+    countStop.removeAttribute("disabled");
+    timerstatus = true;
+    btnstart();
+  }
+  fromTime.value = '';
+};
 
+countStop.onclick = () => {
+  clickstop++;
+  if (countSystem.value == "backwards") {
+    if (clickstop % 2) {
+      clearInterval(backcount);
+      countStop.innerText = "Resume";
+      countStop.classList.remove('btn-warning');
+      countStop.classList.add('btn-success');
+    } else {
+      backward();
+      countStop.innerText = "Stop";
+      countStop.classList.add('btn-warning');
+      countStop.classList.remove('btn-success');
+    }
+  }else{
+    if (clickstop % 2) {
+        clearInterval(backcount);
+        countStop.innerText = "Resume";
+      countStop.classList.remove('btn-warning');
+      countStop.classList.add('btn-success');
+      } else {
+        forward();
+        countStop.innerText = "Stop";
+      countStop.classList.add('btn-warning');
+      countStop.classList.remove('btn-success');
+      } 
+  }
+};
 
+countReset.onclick = () => {
+    countStop.innerText = "Stop";
+    countStop.classList.add('btn-warning');
+    countStop.classList.remove('btn-success');
+    countStop.setAttribute("disabled", "");
+  clearInterval(backcount);
+  timerstatus = false;
+  btnstart();
+  msecnd = 0;
+  secnd = 0;
+  munite = 0;
+  hourr = 0;
+  clickstop = 0;
+  milisec.innerText = `00`;
+  sec.innerText = `00`;
+  minit.innerText = `00`;
+  hour.innerText = `00`;
+};
+function btnstart() {
+  if (timerstatus == true) {
+    countStart.classList.remove("btn-primary");
+    countStart.classList.add("btn-secondary");
+    countStart.setAttribute("disabled", "");
+  } else {
+    countStart.classList.add("btn-primary");
+    countStart.classList.remove("btn-secondary");
+    countStart.removeAttribute("disabled");
+  }
+}
 
-countSystem.addEventListener('change' , () => {
-    (countSystem.value == 'forward')?inputTime.classList.add('d-none'):inputTime.classList.remove('d-none');
-})
+countSystem.addEventListener("change", () => {
+    countReset.click();
+  countSystem.value == "forward"
+    ? inputTime.classList.add("d-none")
+    : inputTime.classList.remove("d-none");
+});
--- assets/js/function.js
+++ assets/js/function.js
@@ -2,17 +2,132 @@
  * get element function
  * return get element
  */
-function qs(value){
-    return document.querySelector(value);
+function qs(value) {
+  return document.querySelector(value);
 }
 
 // Timing function start
-const hour = qs('#hour');
-const minit = qs('#minit');
-const sec = qs('#second');
-const milisec = qs('#milisecond');
-const time = new Date();
+const hour = qs("#hour");
+const minit = qs("#minit");
+const sec = qs("#second");
+const milisec = qs("#milisecond");
+const fromTime = qs("#fromtime");
+const countSystem = qs("#countSystem");
+const inputTime = qs(".input-time");
+const countStart = qs("#countStart");
+const countStop = qs("#countStop");
+const countReset = qs("#countReset");
+let clickstop = 0;
 
-let msec = setInterval( function() {
-    milisec.innerText = time.getMilliseconds();
-},1000)
+let msecnd = 0;
+let secnd = 0;
+let munite = 0;
+let hourr = 0;
+let timerstatus;
+let backcount;
+
+let msec = setInterval(function () {
+  // milisec.innerText = `0${msecnd}`
+  // msecnd ++
+  // if(msecnd == 10){
+  //     msecnd = 0
+  //     secnd ++;
+  //  }else{msecnd;}
+  //  if(secnd == 60){
+  //     secnd = 0;
+  //     munite ++;
+  //  }else{
+  //      secnd = secnd;
+  //  }
+  //  if(munite == 60){
+  //     munite = 0;
+  //     hourr ++;
+  //  }else{
+  //      munite = munite;
+  //  }
+  //  if(hourr > 12){
+  //     hourr = 0;
+  //  }else{
+  //      hourr = hourr;
+  //  }
+}, 100);
+
+/**
+ * munite filter to make houre
+ */
+fromTime.onkeyup = () => {
+  if (fromTime.value >= 60) {
+    hourr = parseInt(fromTime.value / 60);
+    munite = fromTime.value % 60;
+    minit.innerText = `${munite < 10 ? "0" + munite : munite}`;
+    hour.innerText = `${hourr < 10 ? "0" + hourr : hourr}`;
+    fromTime.style.cssText = `
+        border: 1px solid #ced4da;
+    `;
+  } else if (/^[0-9]*$/.test(parseInt(fromTime.value)) != true) {
+    fromTime.style.cssText = `
+        border:2px solid red;
+    `;
+  } else {
+    munite = fromTime.value;
+    hour.innerText = `00`;
+    minit.innerText = `${
+      munite != "" ? (munite < 10 ? "0" + munite : munite) : "00"
+    }`;
+    fromTime.style.cssText = `
+        border: 1px solid #ced4da;
+    `;
+  }
+
+  console.log(/^[0-9]*$/.test(parseInt(fromTime.value)));
+};
+
+function forward() {
+  backcount = setInterval(function () {
+    msecnd++;
+    if (msecnd > 9) {
+      msecnd = 0;
+      secnd++;
+    }
+    if (secnd >= 60) {
+      minit++;
+      hourr++;
+    }
+
+    milisec.innerText = `${msecnd < 10 ? "0" + msecnd : msecnd}`;
+    sec.innerText = `${secnd < 10 ? "0" + secnd : secnd}`;
+    minit.innerText = `${munite < 10 ? "0" + munite : munite}`;
+    hour.innerText = `${hourr < 10 ? "0" + hourr : hourr}`;
+  }, 100);
+}
+
+function backward() {
+  backcount = setInterval(function () {
+    msecnd--;
+    if (hourr > 0 && secnd == 0 && msecnd == 0 && munite == 0) {
+      hourr--;
+      munite = 59;
+      secnd = 59;
+      msecnd = 9;
+    } else if (secnd == 0 && msecnd == 0 && munite > 0) {
+      munite--;
+      secnd = 59;
+      msecnd = 9;
+    } else if (secnd > 0 && msecnd == 0) {
+      secnd--;
+      msecnd = 9;
+    } else if (hourr == 0 && secnd == 0 && msecnd == 0 && munite == 0) {
+      clearInterval(backcount);
+      countStop.setAttribute("disabled", "");
+      timerstatus = false;
+      btnstart();
+    } else if (msecnd < 0) {
+      msecnd = 9;
+    }
+
+    milisec.innerText = `${msecnd < 10 ? "0" + msecnd : msecnd}`;
+    sec.innerText = `${secnd < 10 ? "0" + secnd : secnd}`;
+    minit.innerText = `${munite < 10 ? "0" + munite : munite}`;
+    hour.innerText = `${hourr < 10 ? "0" + hourr : hourr}`;
+  }, 100);
+}
